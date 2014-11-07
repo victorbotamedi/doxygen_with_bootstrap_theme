4398747aaecc35473aff1985072e9b5f988e14a5
victor botamedi <victor.botamedi@gmail.com>
2014-11-07 19:45:46 -0200

    page header

--- doxygen/bootstrap_integration.js
+++ doxygen/bootstrap_integration.js
@@ -2,6 +2,9 @@ $( document ).ready(function()
 {
 	$("div.tabs").addClass("nav nav-tabs");
 
+	$("div.headertitle").addClass("page-header");
+    $("div.title").addClass("h1");
+
 	$(".dynheader").addClass("alert alert-info"); // expand button for class diagrams
 	$(".dyncontent").addClass("panel panel-default");
 
@@ -12,7 +15,6 @@ $( document ).ready(function()
 	$(".deprecated").find(".el")
 		.addClass("label label-danger")
 		.css("margin-bottom", "5px");
-
 	$(".memproto").addClass("panel-heading"); // panel containing a member function prototype
 	$(".memdoc").addClass("panel-body"); // panel containing a member function description
 
@@ -20,7 +22,7 @@ $( document ).ready(function()
 	$("table.directory").addClass("table table-striped");
 	$("table.fieldtable").addClass("table") // tables where elements like enumerations are shown
 
-	$(".contents").addClass("panel panel-primary");
+	$(".contents").addClass("panel panel-default");
 
 	$("table.memberdecls").addClass("table"); // table listing all member functions
 	$("[class^=memitem]").addClass("active");
